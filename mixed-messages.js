3d243e501c06dc99ce66a1bbeb412eaea88cfa20
lenamariola <86557015+lenamariola@users.noreply.github.com>
2021-07-07 21:25:31 +0200

    Updated mixed-messages.js
    now returns a random message depending on the length of the array of messages

--- mixed-messages.js
+++ mixed-messages.js
@@ -8,12 +8,14 @@ messages.push('Schätze die Person, die du bist.')
 messages.push('Mach dir selbst und anderen Mut.')
 messages.push('Freude ist die Essenz des Erfolgs.')
 messages.push('Lerne in Stille, dir selbst zuzuhören.')
+  
 
-console.log(messages)      
 
-const returnMixedMessage = (a) =>  {
-    let a = Math.floor(Math.random(messages.length))
-    return messages[a]
+const returnMixedMessage = (message) => {
+    let a = Math.floor(Math.random()*messages.length)
+    message = messages[a]
+    return message
 }
 
-console.log(returnMixedMessage)
+console.log(returnMixedMessage())
+
